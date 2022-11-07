798920cf09a8a818be8af9099455fb63c10a6064
Jonatan Garcia <jonatangarciabarrera@gmail.com>
2022-11-07 18:43:13 +0100

    hasta 010 completado

--- DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/010accumulator/010accumulator.js
+++ DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/010accumulator/010accumulator.js
@@ -3,16 +3,17 @@ El objeto que crea debería:  Almacenar el “valor actual” en la propiedad va
 inicial se establece en el argumento del constructor startingValue.
 El método read() debe usar prompt para leer un nuevo número y agregarlo a value.
 En otras palabras, la propiedad value es la suma de todos los valores ingresados por el 
-usuario con el valor inicial startingValue.*/
+usuario con el valor inicial startingValue.
+let accumulator = new Accumulator(1); // valor inicial 1
+accumulator.read(); // agrega el valor introducido por el usuario
+accumulator.read(); // agrega el valor introducido por el usuario
+alert(accumulator.value); // muestra la suma de estos valores*/
 "use strict";
 
-let acumulador = new Accumulator(5);
-
-alert(acumulador.value);
-
-acumulador.read();
-
-alert(acumulador.value);
+let accumulator = new Accumulator(1); 
+accumulator.read(); 
+accumulator.read();
+alert(accumulator.value); 
 
 
 function Accumulator(startingValue) {
