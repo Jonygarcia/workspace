4efe8fba1ee94b5f1e1f14430dd5b2b1b1e880fc
Jonatan Garcia <jonatangarciabarrera@gmail.com>
2022-11-12 18:22:41 +0100

    030 mod

--- DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/030arrayToObject/030arrayToObject.js
+++ DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/030arrayToObject/030arrayToObject.js
@@ -10,7 +10,7 @@ Usa el método de array .reduce en la solución.*/
 
 let peliculas = [
     {id:"ESDLA001", nombre: "Frodo Bolsón", pelicula: "El Señor de los Anillos"},
-    {id:"ESDLA002", nombre: "SAM", pelicula: "El Señor de los Anillos"},
+    {id:"ESDLA002", nombre: "Samsagaz Gamyi", pelicula: "El Señor de los Anillos"},
     {id:"HP001", nombre: "Harry Potter", pelicula: "Harry Potter"},
     {id:"HP002", nombre: "Ron Weasley", pelicula: "Harry Potter"},
     {id:"HP003", nombre: "Hermione Granger", pelicula: "Harry Potter"}
