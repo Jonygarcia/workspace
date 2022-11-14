77dea4f16313b7932de5f144d166659dfc4854fc
Jonatan Garcia <jonatangarciabarrera@gmail.com>
2022-11-14 15:53:33 +0100

    donete

--- DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/036formatToRelative/036formatToRelative.js
+++ DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/036formatToRelative/036formatToRelative.js
@@ -9,15 +9,17 @@ decir: "día.mes.año horas:minutos", cada uno de ellos en formato de 2 dígitos
 31.12.16 10:00.*/
 "use strict";
 
-let date = new Date("2022/05/10");
+let date = new Date("2022/11/12 15:53");
 
-formatDate(date);
+alert(formatDate(date));
 
 function formatDate(date) {
     //* Obtener los datos
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
+    let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    let month = date.getMonth() + 1 < 10 ? "0" + date.getMonth() + 1 : date.getMonth() + 1;
     let year = date.getFullYear().toString().slice(2);
+    let hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    let min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
 
     //* Obtener la diferencia de las fecha y tiempo.
     let difDate = new Date() - date;
@@ -30,6 +32,6 @@ function formatDate(date) {
         : difMinutes < 1
             ? "Hace " + difSecs + " segundos"
             : difHours < 1
-                ? "Hace " + difMinutes + " minutos"
-                : "";
+                ? "Hace " + Math.round(difMinutes) + " minutos"
+                : day + "." + month + "." + year + " " + hour + ":" + min;
 }
