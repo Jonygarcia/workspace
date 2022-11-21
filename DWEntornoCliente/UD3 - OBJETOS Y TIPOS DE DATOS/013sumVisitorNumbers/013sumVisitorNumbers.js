09c0f942a2035edc32108d1df5eb8cee6a41a8c8
Jonatan García Barrera <jonatangarciabarrera@gmail.com>
2022-11-21 09:24:46 +0100

    delete donete

--- DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/013sumVisitorNumbers/013sumVisitorNumbers.js
+++
@@ -1,8 +0,0 @@
-/*013sumVisitorNumbers.js/.html: Crea un script que pida al visitante que ingrese dos 
-números y muestre su suma. P.D. Hay una trampa con los tipos de valores.*/
-"use strict";
-
-let num1 = +prompt("Ingrese el primer valor");
-let num2 = +prompt("Ingrese el segundo valor");
-
-alert(num1 + num2);
--- DWEntornoCliente/UD3 - OBJETOS Y TIPOS DE DATOS/031createDate/031createDate.js
+++
@@ -1,7 +0,0 @@
-/*031createDate.js/.html: Crea un objeto Date para la fecha: Feb 20, 2012, 3:12am. La 
-zona horaria es local. Muéstralo en pantalla utilizando alert.*/
-"use strict";
-
-let fecha = new Date("February 20, 2012, 03:12");
-
-alert(fecha);
